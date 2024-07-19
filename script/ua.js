e437a715b1c379e9af2359d79b0c59b5f5c7c1e6
moonfog <65176701+moonfogs@users.noreply.github.com>
2024-07-19 23:31:17 +0800

    Update ua.js

--- script/ua.js
+++ script/ua.js
@@ -1,10 +1,4 @@
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.setRequestHeader('User-Agent', 'Infuse-Direct');
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr.responseText);
-        }
-    };
-    xhr.send();
-    $done(0);
+let headers = $request.headers;
+headers['User-Agent'] = 'Infuse-Direct';
+
+$done({headers});
