c81b43a24cd4b458e1c4f92844b40fa9aba797c9
moonfog <65176701+moonfogs@users.noreply.github.com>
2024-07-23 03:58:44 +0800

    Update ua.js

--- script/ua.js
+++ script/ua.js
@@ -1,5 +1,15 @@
 let headers = $request.headers;
-headers['User-Agent'] = 'Infuse-Direct';
-headers['user-agent'] = 'Infuse-Direct';
 
-$done({headers});
+if ('User-Agent' in headers) {
+    headers['User-Agent'] = 'Infuse-Direct';
+}
+if ('user-agent' in headers) {
+    headers['user-agent'] = 'Infuse-Direct';
+}
+
+let authorizationHeader = 'Emby UserId="612bec78799f4b3a8b52bbaab552096", Client="VidHub_iOS", Device="iPhone", DeviceId="326255D2-24C0-463C-8A0E-C0528A04E427", Version="1.7.4"';
+
+authorizationHeader = authorizationHeader.replace(/Client="[^"]*"/, 'Client="Infuse-Direct"');
+authorizationHeader = authorizationHeader.replace(/Version="[^"]*"/, 'Version="7.8"');
+
+$done({headers, authorizationHeader});
