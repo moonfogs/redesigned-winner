b3d93b552c525a97f76e82560b450ea9a0edad46
moonfog <65176701+moonfogs@users.noreply.github.com>
2024-07-23 05:20:28 +0800

    Update ua.js

--- script/ua.js
+++ script/ua.js
@@ -7,11 +7,19 @@ if ('user-agent' in headers) {
     headers['user-agent'] = 'Infuse-Direct';
 }
 
-if (headers['X-Emby-Authorization']) {
+/*if (headers['X-Emby-Authorization']) {
     let authHeader = headers['X-Emby-Authorization'];
     authHeader = authHeader.replace(/Client="[^"]*"/, 'Client="Infuse-Direct"');
     authHeader = authHeader.replace(/Version="[^"]*"/, 'Version="7.8"');
     headers['X-Emby-Authorization'] = authHeader;
+}*/
+const authHeaderKey = Object.keys(headers).find(key => key.toLowerCase() === 'x-emby-authorization');
+
+if (authHeaderKey) {
+    let authHeader = headers[authHeaderKey];
+    authHeader = authHeader.replace(/Client="[^"]*"/, 'Client="Infuse-Direct"');
+    authHeader = authHeader.replace(/Version="[^"]*"/, 'Version="7.8"');
+    headers[authHeaderKey] = authHeader;
 }
 
 $done({headers});
