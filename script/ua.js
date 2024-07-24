c28aae1c39640df95ba3a72c44f98e1742a53a82
moonfog <65176701+moonfogs@users.noreply.github.com>
2024-07-24 11:07:49 +0800

    更新 ua.js

--- script/ua.js
+++ script/ua.js
@@ -2,7 +2,7 @@ let headers = $request.headers;
 
 ['User-Agent', 'user-agent'].forEach(key => {
     if (key in headers) {
-        headers[key] = "Infuse-Direct";
+        headers[key] = "Infuse-Direct/7.8";
     }
 });
 
@@ -10,8 +10,16 @@ const authHeaderKey = Object.keys(headers).find(key => key.toLowerCase() === 'x-
 
 if (authHeaderKey) {
     let authHeader = headers[authHeaderKey];
+    authHeader = authHeader.replace(/Emby/, 'MediaBrowser');
     authHeader = authHeader.replace(/Client="[^"]*"/, 'Client="Infuse-Direct');
     authHeader = authHeader.replace(/Version="[^"]*"/, 'Version="7.8"');
+
+    const tokenHeaderKey = Object.keys(headers).find(key => key.toLowerCase() === 'x-emby-token');
+    if (tokenHeaderKey) {
+        const token = headers[tokenHeaderKey];
+        authHeader = `${authHeader}, Token="${token}"`;
+    }
+
     headers[authHeaderKey] = authHeader;
 }
 
