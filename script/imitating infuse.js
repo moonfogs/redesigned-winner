55a0ccf8fd0b45010f73846ba827fffe1bc309f6
moonfog <65176701+moonfogs@users.noreply.github.com>
2024-07-26 22:55:59 +0800

    Update imitating infuse.js

--- script/imitating infuse.js
+++ script/imitating infuse.js
@@ -2,7 +2,7 @@ let headers = $request.headers;
 
 ['User-Agent'， 'user-agent'].forEach(key => {
     if (key in headers) {
-        headers[key] = "Infuse-Direct/7.8";
+        headers[key] = "Infuse-Direct/8.0";
     }
 });
 
@@ -12,14 +12,14 @@ if (authHeaderKey) {
     let authHeader = headers[authHeaderKey];
     authHeader = authHeader.replace(/Emby/, 'MediaBrowser');
     authHeader = authHeader.replace(/Client="[^"]*"/, 'Client="Infuse-Direct');
-    authHeader = authHeader.replace(/Version="[^"]*"/, 'Version="7.8"');
+    authHeader = authHeader.replace(/Version="[^"]*"/, 'Version="8.0"');
 
     const tokenHeaderKey = Object.keys(headers).find(key => key.toLowerCase() === 'x-emby-token');
     if (tokenHeaderKey) {
         const token = headers[tokenHeaderKey];
         authHeader = `${authHeader}, Token="${token}"`;
+        delete headers[tokenHeaderKey];
     }
-
     headers[authHeaderKey] = authHeader;
 }
 
