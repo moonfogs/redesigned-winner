37f2f42f034e9ca0f555e188dfbc6aec0fd264a2
moonfog <65176701+moonfogs@users.noreply.github.com>
2024-07-26 22:59:24 +0800

    Update imitating infuse.js

--- script/imitating infuse.js
+++ script/imitating infuse.js
@@ -1,6 +1,7 @@
 let headers = $request.headers;
 
-['User-Agent'， 'user-agent'].forEach(key => {
+const userAgentKeys = ['User-Agent', 'user-agent'];
+userAgentKeys.forEach(key => {
     if (key in headers) {
         headers[key] = "Infuse-Direct/8.0";
     }
@@ -10,9 +11,9 @@ const authHeaderKey = Object.keys(headers).find(key => key.toLowerCase() === 'x-
 
 if (authHeaderKey) {
     let authHeader = headers[authHeaderKey];
-    authHeader = authHeader.replace(/Emby/, 'MediaBrowser');
-    authHeader = authHeader.replace(/Client="[^"]*"/, 'Client="Infuse-Direct');
-    authHeader = authHeader.replace(/Version="[^"]*"/, 'Version="8.0"');
+    authHeader = authHeader.replace(/Emby/, 'MediaBrowser')
+                           .replace(/Client="[^"]*"/, 'Client="Infuse-Direct"')
+                           .replace(/Version="[^"]*"/, 'Version="8.0"');
 
     const tokenHeaderKey = Object.keys(headers).find(key => key.toLowerCase() === 'x-emby-token');
     if (tokenHeaderKey) {
@@ -23,4 +24,4 @@ if (authHeaderKey) {
     headers[authHeaderKey] = authHeader;
 }
 
-$done({headers});
+$done({ headers });
